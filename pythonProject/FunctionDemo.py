e120334f787171e9be3fe8bbe09ce008278d146b
Anupama Saranu <anupama.saranu@cognine.com>
2023-12-07 10:37:27 +0530

    Added changes

--- pythonProject/FunctionDemo.py
+++ pythonProject/FunctionDemo.py
@@ -11,3 +11,17 @@ def addIntegers(a, b):
 
 demo("Anupama")
 print(addIntegers(2,10))
+
+# Fibnonacci Number
+
+
+def Fibonacci(n):
+     if n < 0:
+        print("Incorrect input")
+     elif n == 0:
+        return 0
+     elif n == 1 or n == 2:
+        return 1
+     else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+print(Fibonacci(9))
